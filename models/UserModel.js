dbdd7ad1c2d3d6130d22e1f3933c51784b1f1d67
Hemant Makkar <gautam.makkar2001@gmail.com>
2020-09-30 23:15:24 +0530

    Signup functionality implemented

---
+++ models/UserModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Tell us your name🙏']
+    },
+    email: {
+        type: String,
+        required: [true, 'Tell us your email🙏'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email!']
+    },
+    password: {
+        type: String,
+        required: [true, 'Provide a password🙏'],
+        minlength: 8
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+            validator: function(el) {
+                return el === this.password;
+            },
+            message: 'Passwords are not the same!'
+        }
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
